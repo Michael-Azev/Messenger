f150064931dc8b451774a5bdaa192e2302ecc100
m.azev <m.azev@yandex.ru>
2020-07-14 13:42:04 +0500

    Created new project

---
+++ app/src/main/java/com/azevmikhail/messenger/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.azevmikhail.messenger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
